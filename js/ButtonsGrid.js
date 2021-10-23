9a53fd3edfc52706aa0e4e9eaab127b5dbe5bbb3
Cam Chambers <cam_chambers@outlook.com>
2021-10-23 18:51:04 -0400

    Created a function for rendering tiles dynamically

--- js/ButtonsGrid.js
+++ js/ButtonsGrid.js
@@ -1,14 +1,34 @@
+let size = 4;
+let selected = 0;
+
 let buttonContainer = document.getElementById('btns');
+loadButtons(size);
 
 // Create buttons
-for (let b = 1; b < 17; b++) {
-    var newButton = document.createElement('button');
-    newButton.id = `btn${b}`;
-    newButton.innerHTML = b;
-    newButton.classList.add('btn');
-    newButton.addEventListener('click', function () {
-        alert(this.id);
-    });
-    buttonContainer.append(newButton);
+function loadButtons(n){
+    for (let b = 0; b < n ** 2; b++) {
+        var newButton = document.createElement('button');
+        newButton.id = `btn${b}`;
+        newButton.setAttribute('index', b);
+        newButton.innerHTML = b;
+        newButton.classList.add('btn');
+        newButton.addEventListener('click', function () {
+            swap(this.getAttribute('index'));
+        });
+        buttonContainer.append(newButton);
+    }
 }
 
+// Swap tiles 
+function swap(index){
+    console.log(index);
+    if (index == selected + 1){
+
+    }else if(index == selected - 1){
+
+    }else if(index - n == selected % size){
+
+    }else if (index + n == selected % size ){
+
+    }
+}
